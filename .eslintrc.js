77c4a2f3da1b0f5eb1c181c608ea6b5c08270315
technolog <viktor@allz.su>
2019-04-09 18:40:11 +0300

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,23 @@ module.exports = {
   root: true,
   env: {
     "browser": true,
-    "commonjs": true,
     "node": true,
     "es6": true,
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
+    'plugin:vue/essential'
   ],
   rules: {
+    "no-multiple-empty-lines": [2, { "max": 2, "maxEOF": 1 }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'global-require': 'off',
     'no-new': 0, // Intrusive when using Chart.js instances.
     'no-underscore-dangle': 0, // Chart.js uses underscore dangles (_) internally.
     'import/no-unresolved': 0, // False positives regarding imports that use aliases.
+    'vue/component-name-in-template-casing': ['warn', 'PascalCase', {
+      'ignores': ['i18n']
+    }]
   },
   overrides: [
     {
